e98e398ff1bc1a75d45c81886e2db7220d164789
Phil Peterson <phpeterson@usfca.edu>
2023-01-26 14:09:14 -0800

    add Make demo

--- week01/section02/args.c
+++ week01/section02/args.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printit(char *s);
+
 int main(int argc, char **argv) {
     for (int i = 0; i < argc; i++) 
         printf("argv[%d] = \"%s\"\n", i, argv[i]);
@@ -8,5 +10,7 @@ int main(int argc, char **argv) {
     int base = atoi(argv[1]);
     printf("base = %d\n", base);
 
+    printit("print this!\n");
+    
     return 0;
 }
---
+++ week01/section02/printit.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void printit(char *s) {
+    printf("%s\n", s);
+}
