24040838ce5bec2170b753da5cd92e0844eb361d
Phil Peterson <phpeterson@usfca.edu>
2023-02-09 09:44:53 -0800

    add countc section01

--- week03/section01/add1/add1_c.c
+++ week03/section01/add1/add1_c.c
@@ -1,3 +1,9 @@
-void add1_c(int *pi) {
-    *pi = *pi + 1;
+void add1_c(int *pi, int len) {
+    while (len >= 0) {
+        int n = *pi;
+        n++;
+        *pi = n;
+        len--;
+        pi++;
+    }
 }
